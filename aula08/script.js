ea3d084f9882dd9404a36f2b53f16a969c468ad9
Junior Vieira <139500281+JuniorVcunha@users.noreply.github.com>
2023-09-01 10:18:24 -0300

    ex 008 javascript

---
+++ aula08/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'imagens/manha.png'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#728da0'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/noite.png'
+    } 
+}
